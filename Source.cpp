b975ce7c5537502e5d151903a1577474344a73df
vikhan11 <31761451+vikhan11@users.noreply.github.com>
2017-09-14 20:01:05 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -4,130 +4,170 @@
 using namespace std;
 
 void output(int a[], int size);
-void initialization(int a[], int& size);
-void copy(int a[], int r[], int size);
-
-void main()
+void insertionSort(int *, int size);
+void quickSort(int arr[], int left, int right);
+int main()
 {
-	int menu,n;
-	int *A = new int[0];
-	int *R = new int[0];
-	double start_time, end_time;
-	srand(time(NULL));
+    int menu, n;
+    int *A = new int[0];
+    int *R = new int[0];
+    double start_time, end_time;
+    srand(time(NULL));
+    
+    cout << "Menu:" << endl;
+    cout << "1." << endl;
+    cout << "2." << endl;
+    cout << "3." << endl;
+    cout << "4." << endl;
+    cout << "5." << endl;
+    cout << "6.End of work" << endl;
+    
+    int check;
+    cout << "Size of your vector ";
+    cin >> n;
+    
+    cout << "Random vector? ";
+    cin >> check;
+    A = new int[n];
+    
+    if (check != 1)
+    {
+        cout << "Please input your vector" << endl;
+        
+        for (int i = 0; i < n; ++i)
+        {
+            cin >> A[i];
+        }
+    }
+    else
+    {
+        for (int i = 0; i < n; ++i)
+        {
+            A[i] = rand();
+        }
+    }
+    
+    R = new int[n];
+    
+    do
+    {
+        cout << "Menu item? ";
+        cin >> menu;
+        
+        if (menu <= 5 && menu >= 1)
+        {
+            for (int i = 0; i < n; ++i)
+            {
+                R[i] = A[i];
+            }
+            cout << "Before sorting" << endl;
+            output(A, n);
+            start_time = clock();
+        }
+        switch (menu)
+        {
+            case 1:
+            {
+                
+                break;
+            }
+            case 2:
+            {
+                quickSort(A, 0, n-1);
 
-	cout << "Menu:" << endl;
-	cout << "1." << endl;
-	cout << "2." << endl;
-	cout << "3." << endl;
-	cout << "4." << endl;
-	cout << "5." << endl;
-	cout << "6.End of work" << endl;
-
-	initialization(A,n);
-
-	do
-	{
-		cout << "Menu item? ";
-		cin >> menu;
-
-		if (menu <= 5 && menu >= 1)
-		{
-			copy(A, R, n);
-			cout << "Before sorting" << endl;
-			output(A, n);
-			start_time = clock();
-		}
-		switch (menu)
-		{
-		case 1:
-		{
-
-			break;
-		}
-		case 2:
-		{
-
-			break;
-		}
-		case 3:
-		{
-
-			break;
-		}
-		case 4:
-		{
-
-			break;
-		}
-		case 5:
-		{
-
-			break;
-		}
-		case 6:
-		{
-			delete[] A; delete[] R;
-			cout << "Thanks for choosing our product." << endl;
-			break;
-		}
-		default:
-		{
-			cout << "Please input proper value" << endl;
-			break;
-		}
-		}
-		if (menu <= 5 && menu >= 1) 
-		{
-			end_time = clock();
-			cout << "After sorting" << endl;
-			output(R, n);
-			cout << "And it was done in " << end_time / CLOCKS_PER_SEC - start_time / CLOCKS_PER_SEC << endl;
-		}
-	} while (menu != 6);
-
-	system("pause");
+                break;
+            }
+            case 3:
+            {
+                insertionSort(R, n);
+                break;
+            }
+            case 4:
+            {
+                
+                break;
+            }
+            case 5:
+            {
+                
+                break;
+            }
+            case 6:
+            {
+                delete[] A; delete[] R;
+                cout << "Thanks for choosing our product." << endl;
+                break;
+            }
+            default:
+            {
+                cout << "Please input proper value" << endl;
+                break;
+            }
+        }
+        if (menu <= 5 && menu >= 1)
+        {
+            end_time = clock();
+            cout << "After sorting" << endl;
+            output(R, n);
+            cout << "And it was done in " << end_time / CLOCKS_PER_SEC - start_time / CLOCKS_PER_SEC << endl;
+        }
+    } while (menu != 6);
+    
+    system("pause");
 }
 
 void output(int a[], int size)
 {
-	for (int i = 0; i < size; ++i)
-	{
-		cout << a[i] << ' ';
-	}
-	cout << endl;
+    for (int i = 0; i < size; ++i)
+    {
+        cout << a[i] << ' ';
+    }
+    cout << endl;
 }
-void initialization(int a[], int& size)
+void insertionSort(int *array, int size)
 {
-	int check;
-	cout << "Size of your vector ";
-	cin >> size;
-
-	cout << "Random vector? ";
-	cin >> check;
-	a = new int[size];
-
-	if (check == 0)
-	{
-		cout << "Please input your vector" << endl;
-
-		for (int i = 0; i < size; ++i)
-		{
-			cin >> a[i];
-		}
-	}
-	else
-	{
-		for (int i = 0; i < size; ++i)
-		{
-			a[i] = rand();
-		}
-	}
+    int temp, item;
+    for (int i = 1; i < size; i++)
+    {
+        temp = array[i];
+        item = i - 1;
+        while (item >= 0 && array[item] > temp)
+        {
+            array[item + 1] = array[item];
+            array[item] = temp;
+            item--;
+        }
+    }
 }
-void copy(int a[], int r[], int size) 
-{
-	r = new int[size];
-	for (int i = 0; i < size; ++i)
-	{
-		r[i] = a[i];
-	}
+
+void quickSort(int arr[], int left, int right) {
+    int i = left, j = right;
+    int tmp;
+    int mid = arr[(left + right) / 2];
+
+    while (i <= j) {
+        while (arr[i] < mid)
+            i++;
+        while (arr[j] > mid)
+            j--;
+        if (i <= j) {
+            tmp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = tmp;
+            i++;
+            j--;
+        }
+    }
+    if (left < j){
+        
+        quickSort(arr, left, j);
+    }
+    if (i < right){
+        
+        quickSort(arr, i, right);
+    }
+    if (i >= right && left >= j){
+        
+    }
+    
 }
