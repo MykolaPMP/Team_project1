298af449515b453841e7bccb64d8ff7543907411
orko98 <31936833+orko98@users.noreply.github.com>
2017-09-14 18:07:05 +0300

    Update Source.cpp
    змінив трох інтерфес , думаю це буде хороший робочий варіант + своє сортування

--- Source.cpp
+++ Source.cpp
@@ -5,11 +5,10 @@ using namespace std;
 
 void output(int a[], int size);
 void initialization(int a[], int& size);
-void copy(int a[], int r[], int size);
 
 void main()
 {
-	int menu,n;
+	int menu, n;
 	int *A = new int[0];
 	int *R = new int[0];
 	double start_time, end_time;
@@ -23,17 +22,22 @@ void main()
 	cout << "5." << endl;
 	cout << "6.End of work" << endl;
 
-	initialization(A,n);
+	initialization(A, n);
+	R = new int[n];
 
 	do
 	{
-		cout << "Menu item? ";
+		cout << "Choose Menu item  ";
 		cin >> menu;
 
 		if (menu <= 5 && menu >= 1)
 		{
-			copy(A, R, n);
-			cout << "Before sorting" << endl;
+			for (int i = 0; i < n; ++i)
+			{
+				cout << i;
+				R[i] = A[i];
+			}
+			cout << "Before sorting :  " ;
 			output(A, n);
 			start_time = clock();
 		}
@@ -41,7 +45,66 @@ void main()
 		{
 		case 1:
 		{
+			int *b = new int[0];
+			copy(A, b, n);
+			int  c;
+			int m = n / 2;
+			for (int i = 0; i<m - 1; i++)
+			{
+				for (int j = 0; j<m - i - 1; j++)
+				{
+					if (b[j]>b[j + 1]) {
+						c = b[j];
+						b[j] = b[j + 1];
+						b[j + 1] = c;
+					}
+				}
+			}
+			for (int i = m; i<n - 1; i++) {
+				for (int j = m; j<n - (i - m) - 1; j++)
+				{
+					if (b[j]>b[j + 1]) {
+						c = b[j];
+						b[j] = b[j + 1];
+						b[j + 1] = c;
+					}
+				}
+			}
+			for (int i = 0; i<n; cout << b[i] << ends, i++);
+			cout << endl;
+			int l = 0, r = m, k = 0;
 
+			while (k<n)
+			{
+				if (l<m - 1 && r<m)
+				{
+					if (b[l]<b[r])
+					{
+						R[k] = b[l];
+						++l;
+					}
+					else
+					{
+						R[k] = b[r]; ++r;
+					}
+				}
+				else
+				{
+					if (l<m - 1)
+					{
+						R[k] = b[l];
+						l++;
+					}
+					else
+					{
+						R[k] = b[r];
+						r++;
+					}
+				}
+				k++;
+			}
+
+			delete[] b;
 			break;
 		}
 		case 2:
@@ -67,21 +130,21 @@ void main()
 		case 6:
 		{
 			delete[] A; delete[] R;
-			cout << "Thanks for choosing our product." << endl;
+			cout << "Thanks for choosing our product ^.^ " << endl;
 			break;
 		}
 		default:
 		{
-			cout << "Please input proper value" << endl;
+			cout << "Please input proper value " << endl;
 			break;
 		}
 		}
-		if (menu <= 5 && menu >= 1) 
+		if (menu <= 5 && menu >= 1)
 		{
 			end_time = clock();
 			cout << "After sorting" << endl;
 			output(R, n);
-			cout << "And it was done in " << end_time / CLOCKS_PER_SEC - start_time / CLOCKS_PER_SEC << endl;
+			cout << "And it was done in " << end_time / CLOCKS_PER_SEC - start_time / CLOCKS_PER_SEC << " sec ";
 		}
 	} while (menu != 6);
 
@@ -102,7 +165,7 @@ void initialization(int a[], int& size)
 	cout << "Size of your vector ";
 	cin >> size;
 
-	cout << "Random vector? ";
+	cout << "Enter 1 , to randomize vector ";
 	cin >> check;
 	a = new int[size];
 
@@ -121,13 +184,5 @@ void initialization(int a[], int& size)
 		{
 			a[i] = rand();
 		}
-	}
-}
-void copy(int a[], int r[], int size) 
-{
-	r = new int[size];
-	for (int i = 0; i < size; ++i)
-	{
-		r[i] = a[i];
 	}
 }
