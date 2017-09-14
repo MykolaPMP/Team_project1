425609754b7b15e5a8e5ee692bb52c69c5574cd5
MykolaPMP <31858361+MykolaPMP@users.noreply.github.com>
2017-09-14 19:16:57 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -4,12 +4,11 @@
 using namespace std;
 
 void output(int a[], int size);
-void initialization(int a[], int& size);
-void copy(int a[], int r[], int size);
+void insertionSort(int *, int size);
 
 void main()
 {
-	int menu,n;
+	int menu, n;
 	int *A = new int[0];
 	int *R = new int[0];
 	double start_time, end_time;
@@ -18,12 +17,37 @@ void main()
 	cout << "Menu:" << endl;
 	cout << "1." << endl;
 	cout << "2." << endl;
-	cout << "3." << endl;
+	cout << "3. Insertion sort" << endl;
 	cout << "4." << endl;
 	cout << "5." << endl;
 	cout << "6.End of work" << endl;
 
-	initialization(A,n);
+	int check;
+	cout << "Size of your vector ";
+	cin >> n;
+
+	cout << "Random vector? ";
+	cin >> check;
+	A = new int[n];
+
+	if (check != 1)
+	{
+		cout << "Please input your vector" << endl;
+
+		for (int i = 0; i < n; ++i)
+		{
+			cin >> A[i];
+		}
+	}
+	else
+	{
+		for (int i = 0; i < n; ++i)
+		{
+			A[i] = rand();
+		}
+	}
+
+	R = new int[n];
 
 	do
 	{
@@ -32,7 +56,10 @@ void main()
 
 		if (menu <= 5 && menu >= 1)
 		{
-			copy(A, R, n);
+			for (int i = 0; i < n; ++i)
+			{
+				R[i] = A[i];
+			}
 			cout << "Before sorting" << endl;
 			output(A, n);
 			start_time = clock();
@@ -51,7 +78,8 @@ void main()
 		}
 		case 3:
 		{
-
+			cout << "Insertion sort" << endl;
+			insertionSort(R, n); 
 			break;
 		}
 		case 4:
@@ -76,7 +104,7 @@ void main()
 			break;
 		}
 		}
-		if (menu <= 5 && menu >= 1) 
+		if (menu <= 5 && menu >= 1)
 		{
 			end_time = clock();
 			cout << "After sorting" << endl;
@@ -96,38 +124,18 @@ void output(int a[], int size)
 	}
 	cout << endl;
 }
-void initialization(int a[], int& size)
+void insertionSort(int *array, int size)
 {
-	int check;
-	cout << "Size of your vector ";
-	cin >> size;
-
-	cout << "Random vector? ";
-	cin >> check;
-	a = new int[size];
-
-	if (check == 0)
+	int temp, item;
+	for (int i = 1; i < size; i++)
 	{
-		cout << "Please input your vector" << endl;
-
-		for (int i = 0; i < size; ++i)
+		temp = array[i];
+		item = i - 1;
+		while (item >= 0 && array[item] > temp)
 		{
-			cin >> a[i];
+			array[item + 1] = array[item];
+			array[item] = temp;
+			item--;
 		}
-	}
-	else
-	{
-		for (int i = 0; i < size; ++i)
-		{
-			a[i] = rand();
-		}
-	}
-}
-void copy(int a[], int r[], int size) 
-{
-	r = new int[size];
-	for (int i = 0; i < size; ++i)
-	{
-		r[i] = a[i];
 	}
 }
